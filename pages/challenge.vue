bc0a90bb892d4a19c9edac006a5e2bc736c4b39e
Luis Lovos <luisestebanlovos@gmail.com>
2025-03-05 21:02:37 -0300

    Update

--- pages/challenge.vue
+++ pages/challenge.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
         <h1>Page Challenge</h1>
+        <Challenge :slides="slides" :autoplay="true" :autoplayDelay="5000"/>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            slides: [] // Define slides here
+        };
+    }
+};
+</script>
